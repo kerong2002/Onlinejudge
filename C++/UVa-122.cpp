a8eb8f475244f3c3e8a885b4ca3018772c540571
kerong <70834651+kerong2002@users.noreply.github.com>
2023-06-04 14:54:14 +0800

    Add files via upload

--- C++/UVa-122.cpp
+++ C++/UVa-122.cpp
@@ -15,10 +15,10 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {} //完整資訊的建構子
 };
 
-int string_to_int(string str){
+int string_to_int(string str) {
     int power = 1;
     int ans = 0;
-    for(int x=str.size()-1;x>=1;x--){
+    for(int x=str.size()-1; x>=1; x--) {
         ans += (str[x]-'0') * power;
         power *= 10;
     }
@@ -27,40 +27,40 @@ int string_to_int(string str){
 
 
 //========<每層打印>==============
-void level_order(TreeNode *root, const int max_node_cnt){
+void level_order(TreeNode *root, const int max_node_cnt) {
     int root_cnt = 1;
     queue<TreeNode *> q;
     q.push(root);
-    while(!q.empty()){
-        if(root_cnt++ > max_node_cnt){
+    while(!q.empty()) {
+        if(root_cnt++ > max_node_cnt) {
             cout << q.front()->val;
         }
-        else{
+        else {
             cout << q.front()->val << " ";
         }
-        if(q.front()->left){
+        if(q.front()->left) {
             q.push(q.front()->left);
         }
-        if(q.front()->right){
+        if(q.front()->right) {
             q.push(q.front()->right);
         }
         q.pop();
     }
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     string buffer;          //把切割的緩衝
     string val_data;        //切割後數值
-    while(true){
+    while(true) {
         TreeNode *head = new TreeNode();
         int root_count = -1;          //節點填滿的數量
         bool is_equal = false;        //重複節點問題
         bool end_test = true;
         int node_cnt = 0;
-        while(cin>>buffer){
+        while(cin>>buffer) {
             end_test = false;
             //=======<終止條件>===========//
-            if(buffer.size()==2){
+            if(buffer.size()==2) {
                 break;
             }
             //======<根據節點資料，做處理>=======//
@@ -77,61 +77,61 @@ int main(int argc, char **argv){
 
             TreeNode *node = head;                //獲取頭資料
 
-            if(root_position.size() == 1){          //最根部處理
-                if(node->val == -1){
+            if(root_position.size() == 1) {         //最根部處理
+                if(node->val == -1) {
                     node->val = root_val;
                     root_count += 1;                //找到根部填充
                 }
-                else{
+                else {
                     is_equal = true;                //找到重複根部
                 }
             }
-            else{
-                for(int x=0;x<root_position.size()-1;x++){
-                    if(root_position[x]=='L'){                  //往左探索
-                        if(node->left==NULL){
+            else {
+                for(int x=0; x<root_position.size()-1; x++) {
+                    if(root_position[x]=='L') {                 //往左探索
+                        if(node->left==NULL) {
                             TreeNode *temp = new TreeNode();
                             node->left = temp;
                             node = node->left;
                             root_count -= 1;                    //空的節點
                         }
-                        else{
+                        else {
                             node = node->left;
                         }
                     }
-                    else if(root_position[x]=='R'){             //往右探索
-                        if(node->right==NULL){
+                    else if(root_position[x]=='R') {            //往右探索
+                        if(node->right==NULL) {
                             TreeNode *temp = new TreeNode();
                             node->right = temp;
                             node = node->right;
                             root_count -= 1;                    //空的節點
                         }
-                        else{
-                           node = node->right;
+                        else {
+                            node = node->right;
                         }
                     }
                 }
 
                 //======<放到沒有填充的位置>=======//
-                if(node->val == -1){
+                if(node->val == -1) {
                     node->val = root_val;
                     root_count += 1;
                     node_cnt += 1;
                 }
-                else{
+                else {
                     is_equal = true;                //重複的
                 }
             }
         }
 
-        if(end_test){
+        if(end_test) {
             break;
         }
         //======<存在重複節點>======//
-        if(is_equal || root_count!=0){
+        if(is_equal || root_count!=0) {
             cout << "not complete" << endl;
         }
-        else{
+        else {
             //postorder(head);
             level_order(head, node_cnt);
             cout << endl;
